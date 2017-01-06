a52ba0847b75188df7ff248139c1e6115e568f43
lino@rmacbookpro <castlino@gmail.com>
2017-01-06 13:51:39 +1100

    Added all files.

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+  echo "<p>local vagrant-ansible box...</p>";
+  echo "<p>lino here...</p>";
+  echo "<p>Reference:</p>";
+  echo "<p>&nbsp;&nbsp;<a href='https://adamcod.es/2014/09/23/vagrant-ansible-quickstart-tutorial.html'>https://adamcod.es/2014/09/23/vagrant-ansible-quickstart-tutorial.html</a></p>";
+?>
